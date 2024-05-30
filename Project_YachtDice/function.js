0455f5e20926a6cfee7661c0e741839dea61016a
haralee <128335451+Hara-JS@users.noreply.github.com>
2024-05-30 14:01:41 +0900

    YachtDice

---
+++ Project_YachtDice/function.js
@@ -0,0 +1,49 @@
+    function randomDice() {
+    }
+
+    function Aces() {
+    }
+
+    function Twos() {
+    }
+
+    function Threes() {
+    }
+
+    function Fours() {
+    }
+
+    function Fives() {
+    }
+
+    function sixes() {
+    }
+
+    function totalScore() {
+    }
+
+    function bonusScore() {
+        if(totalScore >= 63) {
+            totalScore += 35;
+        } else {
+            totalScore = totalScore;
+        }
+    }
+
+    function Choice() {
+    }
+
+    function fourOfAKind() {
+    }
+
+    function fullHouse() {
+    }
+
+    function smallStraight() {
+    }
+
+    function LargeStraight() {
+    }
+
+    function yacht() {
+    }
